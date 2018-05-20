c0225d07b5f3fc7c9708b7123621188c92234526
Nguyen Khac Nhat <nhat@agilead.vn>
2018-05-20 17:23:21 +0700

    First commit

---
+++ src/main/java/com/codegym/formatter/ApplicationConfig.java
@@ -0,0 +1,31 @@
+package com.codegym.formatter;
+
+import com.codegym.formatter.com.codegym.formatter.formatter.LocalDateFormatter;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.format.FormatterRegistry;
+import org.springframework.web.servlet.ViewResolver;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+import org.springframework.web.servlet.view.InternalResourceViewResolver;
+
+@Configuration
+@EnableWebMvc
+@ComponentScan("com.codegym.formatter")
+public class ApplicationConfig extends WebMvcConfigurerAdapter {
+
+    @Bean
+    public ViewResolver viewResolver(){
+        InternalResourceViewResolver viewResolver = new InternalResourceViewResolver();
+        viewResolver.setPrefix("/WEB-INF/views/");
+        viewResolver.setSuffix(".jsp");
+        return viewResolver;
+    }
+
+    @Override
+    public void addFormatters(FormatterRegistry registry) {
+        LocalDateFormatter localDateFormatter = new LocalDateFormatter("MM-dd-yyyy");
+        registry.addFormatter(localDateFormatter);
+    }
+}
